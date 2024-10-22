9d706c7ed27d1b04c1c5890412830695251560bf
sandra-mastropasqua <sandra.mastropasqua@laplateforme.io>
2024-10-22 16:15:16 +0200

    Tic tac toe

---
+++ tictactoe.py
@@ -0,0 +1,47 @@
+def utilisateur (Joueur1,Joueur2):
+    print(input("Le joueur 1 choisit son symbole soit X ou O : "))
+    if Joueur1 == "X":
+        print("Vous avez choisis le symbole X")
+    elif Joueur1 != "X":
+        print("Vous avez choisis le symbole O")
+    for O in Joueur2 : 
+        if Joueur1 == "X":
+            print("Le joueur 2 sera alors O")
+        elif Joueur1 == "O":
+            print("Le joueur 2 sera alors X")
+utilisateur("X","O")
+a=0
+b=1
+c=2
+d=3
+e=4
+f=5
+g=6
+h=7
+i=8
+print("_____"*5)
+print("|"+"___"+f"{a}"+"___"+"|"+"___"+f"{b}"+"___"+"|"+"___"+f"{c}"+"___"+"|")
+print("|"+"___"+f"{d}"+"___"+"|"+"___"+f"{e}"+"___"+"|"+"___"+f"{f}"+"___"+"|")
+print("|"+"___"+f"{g}"+"___"+"|"+"___"+f"{h}"+"___"+"|"+"___"+f"{i}"+"___"+"|")
+
+numerocase= [a,b,c,d,e,f,g,h,i]
+for numerocase in range(0,8):
+    if input(numerocase7):
+        
+        X = a = 0
+        print(5)
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
