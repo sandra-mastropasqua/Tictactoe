40af199674874e0093d3b87cc5673500c9732632
sandra-mastropasqua <sandra.mastropasqua@laplateforme.io>
2024-10-23 09:52:43 +0200

    Choix des joueurs

--- tictactoe.py
+++ tictactoe.py
@@ -1,15 +1,21 @@
-def utilisateur (Joueur1,Joueur2):
-    print(input("Le joueur 1 choisit son symbole soit X ou O : "))
-    if Joueur1 == "X":
-        print("Vous avez choisis le symbole X")
-    elif Joueur1 != "X":
-        print("Vous avez choisis le symbole O")
-    for O in Joueur2 : 
-        if Joueur1 == "X":
-            print("Le joueur 2 sera alors O")
-        elif Joueur1 == "O":
-            print("Le joueur 2 sera alors X")
-utilisateur("X","O")
+def utilisateur():
+    symbole = ""
+    X = ''
+    O = ''
+    while symbole == X and symbole == O :
+        symbole = input("Choisissez joueur 1 votre symbole: ")
+    def utilisateur2():
+        symbole_joueur2 = ''
+        X = ''
+        O = ''
+        while symbole_joueur2 == X and symbole_joueur2 == O :
+            symbole_joueur2 = input(f"Choisissez joueur 2 votre symbole: ")
+            if symbole_joueur2 == symbole :
+                print(f"Le joueur 1 a deja choisis {symbole}. Choisissez l'autre symbole.")
+        return symbole_joueur2
+    utilisateur2()
+utilisateur()
+
 a=0
 b=1
 c=2
@@ -24,12 +30,6 @@ print("|"+"___"+f"{a}"+"___"+"|"+"___"+f"{b}"+"___"+"|"+"___"+f"{c}"+"___"+"|")
 print("|"+"___"+f"{d}"+"___"+"|"+"___"+f"{e}"+"___"+"|"+"___"+f"{f}"+"___"+"|")
 print("|"+"___"+f"{g}"+"___"+"|"+"___"+f"{h}"+"___"+"|"+"___"+f"{i}"+"___"+"|")
 
-numerocase= [a,b,c,d,e,f,g,h,i]
-for numerocase in range(0,8):
-    if input(numerocase7):
-        
-        X = a = 0
-        print(5)
 
 
 
