b4c4b1178b70f172c333e0aec1280701767977a4
e-mellinandrichier <emmanuelle.mellinand-richier@laplateforme.io>
2024-10-24 14:43:08 +0200

    numero a la place des index

--- testemma.py
+++ testemma.py
@@ -1,29 +1,26 @@
-
 empty = ' '
 matrice = [
     [empty for i in range(3)],
     [empty for i in range(3)],
     [empty for i in range(3)]
 ]
-
+print("Bienvenue dans le tictactoe de Muriel, Sandra et Emmanuelle :)")
 player  = input('Entrez le signe du joueur 1 (x ou o) : ')
 if player not in ['x', 'o']:
     print('erreur')
     exit()
-    
+
 def winorlose():
     counter = 0
-    print("Bienvenue dans le tictactoe de Muriel, Sandra et Emmanuelle :)")
-    print("La grille de jeu est agencée avec 3 lignes et 3 colonnes. Voici les index de chaque case :")
+    print("La grille de jeu est agencée en 9 cases. Voici les numéros de chaque case :")
     print("_____"*5)
-    print("|"+"  "+"0,0"+"  "+"|"+"  "+"0,1"+"  "+"|"+"  "+"0,2"+"  "+"|")
+    print("|"+"   "+"1"+"   "+"|"+"   "+"2"+"   "+"|"+"   "+"3"+"   "+"|")
     print("|"+"_______"+"|"+"_______"+"|"+"_______"+"|")
-    print("|"+"  "+"1,0"+"  "+"|"+"  "+"1,1"+"  "+"|"+"  "+"1,2"+"  "+"|")
+    print("|"+"   "+"4"+"   "+"|"+"   "+"5"+"   "+"|"+"   "+"6"+"   "+"|")
     print("|"+"_______"+"|"+"_______"+"|"+"_______"+"|")
-    print("|"+"  "+"2,0"+"  "+"|"+"  "+"2,1"+"  "+"|"+"  "+"2,2"+"  "+"|")
+    print("|"+"   "+"7"+"   "+"|"+"   "+"8"+"   "+"|"+"   "+"9"+"   "+"|")
     print("|"+"_______"+"|"+"_______"+"|"+"_______"+"|")
 
-   
     for row in matrice:
         for case in row:
             c = 0
@@ -31,9 +28,18 @@ def winorlose():
                 x_wins(c)
                 o_wins(c)
                 c +=1
-            ligne = int(input("Dans quel rang voulez vous jouer (0, 1 ou 2)?"))
-            col = int(input(f"Vous jouez dans le rang {ligne}, quelle case voulez vous jouer (0, 1 ou 2)?"))
-
+            case = int(input("entre le num de la case "))
+            i = 0
+            counter = 1
+            while i <= 2:
+                j = 0
+                while j <= 2:
+                    if case == counter:
+                        ligne = i
+                        col = j
+                    counter+=1
+                    j +=1
+                i +=1
             mystr = ''.join(matrice[0] + matrice[1] + matrice[2])
             counter_x = mystr.count('X')
             counter_o = mystr.count('O')
@@ -87,7 +93,7 @@ def x_wins(c):
         x_is_player1()
         exit()
     if matrice [0][2] == matrice [1][1] == matrice [2][0] == "X":
-        x_is_player1
+        x_is_player1()
         exit()
 
 def o_wins(c):
@@ -97,7 +103,7 @@ def o_wins(c):
         exit()
     #ligne
     if matrice[c] == ["O","O","O"] :
-        o_is_player1
+        o_is_player1()
         exit()
     #diagonales
     if matrice [0][0] == matrice [1][1] == matrice [2][2] == "O":
